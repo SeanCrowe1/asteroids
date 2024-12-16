85ea686a9040e3d59c51c7879dc846130dc65b13
Sean Crowe <sean.crowe2002@gmail.com>
2024-12-16 06:32:06 +0000

    Player object created

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
-    pygame.init
+    pygame.init()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000, rect=None, special_flags=0)
+        screen.fill((0, 0, 0), rect=None, special_flags=0)
+        ship.draw(screen)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
