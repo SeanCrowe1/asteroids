ff47d580eb486ed7f2ca6ba4733070173298d41f
SeanCrowe1 <sean.crowe2002@gmail.com>
2025-11-09 20:39:22 +0000

    player object created and movable

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     print(f"Starting Asteroids! with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
+
+        player.update(dt)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
